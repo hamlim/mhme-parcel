79411d1bf4db0cd84783ce641892e01ea8bb7238
Matt Hamlin <hamlim@outlook.com>
2018-03-31 17:21:38 -0400

    Update more paths

--- src/pages/blog/2016/September/september.js
+++ src/pages/blog/2016/September/september.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Post from '../../../../components/post.js'
 
 const md = require('fs').readFileSync(
-  __dirname + './september.md',
+  __dirname + '/september.md',
   { encoding: 'utf-8' },
 )
 
--- src/pages/blog/2016/September/sick.js
+++ src/pages/blog/2016/September/sick.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Post from '../../../../components/post.js'
 
 const md = require('fs').readFileSync(
-  __dirname + './sick.md',
+  __dirname + '/sick.md',
   { encoding: 'utf-8' },
 )
 
