276c25ff04c8699306307fd3a08f4345739c3c12
Matt Hamlin <hamlim@outlook.com>
2018-07-21 08:51:30 -0700

    Updating deps, attempting to use imagemin plugin

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
 import React, { Fragment } from 'react'
 import { render } from 'react-dom'
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from 'react-router-dom'
-import {
-  TransitionGroup,
-  CSSTransition,
-} from 'react-transition-group'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import setup from './setup'
 
 import Navigation from './components/navigation.js'
 
 import Landing from './pages/landing.js'
-import Blog from './pages/blog/index.js'
-import Projects from './pages/projects.js'
-import ReactStateReducer from './pages/react-state-reducer'
-import Recycles from './pages/recycles.js'
-import FourOhFour from './pages/404.js'
+
+class Lazy extends React.Component {
+  // Rendering an empty span because of this bug in react transition group
+  // https://github.com/reactjs/react-transition-group/issues/208
+  state = { mod: { default: () => <span /> } }
+  componentDidMount() {
+    this.props
+      .mod()
+      .then(mod => {
+        this.setState({ mod })
+      })
+      .catch(console.error)
+  }
+  render() {
+    return this.props.children(this.state)
+  }
+}
 
 const App = () => (
   <Router>
@@ -28,31 +33,46 @@ const App = () => (
         render={({ location }) => (
           <Fragment>
             <TransitionGroup>
-              <CSSTransition
-                key={location.key}
-                classNames="fade"
-                timeout={300}
-              >
+              <CSSTransition key={location.key} classNames="fade" timeout={300}>
                 <Switch location={location}>
-                  <Route
-                    path="/"
-                    exact
-                    component={Landing}
-                  />
-                  <Route path="/blog" component={Blog} />
-                  <Route
-                    path="/projects"
-                    component={Projects}
-                  />
-                  <Route
-                    path="/react-state-reducer"
-                    component={ReactStateReducer}
-                  />
-                  <Route
-                    path="/recycles"
-                    component={Recycles}
-                  />
-                  <Route component={FourOhFour} />
+                  <Route path="/" exact component={Landing} />
+                  <Route path="/blog">
+                    {rProps => (
+                      <Lazy mod={() => import('./pages/blog/index.js')}>
+                        {({ mod }) => <mod.default {...rProps} />}
+                      </Lazy>
+                    )}
+                  </Route>
+                  <Route path="/projects">
+                    {rProps => (
+                      <Lazy mod={() => import('./pages/projects.js')}>
+                        {({ mod }) => <mod.default {...rProps} />}
+                      </Lazy>
+                    )}
+                  </Route>
+                  <Route path="/react-state-reducer">
+                    {rProps => (
+                      <Lazy
+                        mod={() => import('./pages/react-state-reducer.js')}
+                      >
+                        {({ mod }) => <mod.default {...rProps} />}
+                      </Lazy>
+                    )}
+                  </Route>
+                  <Route path="/recycles">
+                    {rProps => (
+                      <Lazy mod={() => import('./pages/recycles.js')}>
+                        {({ mod }) => <mod.default {...rProps} />}
+                      </Lazy>
+                    )}
+                  </Route>
+                  <Route>
+                    {rProps => (
+                      <Lazy mod={() => import('./pages/404.js')}>
+                        {({ mod }) => <mod.default {...rProps} />}
+                      </Lazy>
+                    )}
+                  </Route>
                 </Switch>
               </CSSTransition>
             </TransitionGroup>
--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,45 +1,93 @@
-import React, { Fragment } from 'react'
-import { Route, Link } from 'react-router-dom'
-import Breadcrumbs from '../../components/breadcrumbs.js'
+import React, { Fragment, Component } from 'react'
 import Container from '../../components/container.js'
 
-import TwentyFifteen from './2015/index.js'
-import TwentySixteen from './2016/index.js'
-import TwentySeventeen from './2017/index.js'
-import TwentyEighteen from './2018/index.js'
+// Rendering an empty span because of this bug in react transition group
+// https://github.com/reactjs/react-transition-group/issues/208
+const Null = () => <span />
 
-const Blog = () => (
-  <Container>
-    <Breadcrumbs />
-    <Route path="/blog/2015" component={TwentyFifteen} />
-    <Route path="/blog/2016" component={TwentySixteen} />
-    <Route path="/blog/2017" component={TwentySeventeen} />
-    <Route path="/blog/2018" component={TwentyEighteen} />
-    <Route
-      path="/blog"
-      exact
-      render={r =>
-        r.match && (
-          <Fragment>
-            <h3>Years:</h3>
-            <ul>
-              <li>
-                <Link to="/blog/2015">2015</Link>
-              </li>
-              <li>
-                <Link to="/blog/2016">2016</Link>
-              </li>
-              <li>
-                <Link to="/blog/2017">2017</Link>
-              </li>
-              <li>
-                <Link to="/blog/2018">2018</Link>
-              </li>
-            </ul>
-          </Fragment>
-        )
-      }
-    />
-  </Container>
-)
+class Blog extends Component {
+  state = {
+    Route: Null,
+    Link: Null,
+    Breadcrumbs: Null,
+    TwentyFifteen: Null,
+    TwentySixteen: Null,
+    TwentySeventeen: Null,
+    TwentyEighteen: Null,
+  }
+  componentDidMount() {
+    Promise.all([
+      import('react-router-dom'),
+      import('../../components/breadcrumbs.js'),
+      import('./2015/index.js'),
+      import('./2016/index.js'),
+      import('./2017/index.js'),
+      import('./2018/index.js'),
+    ]).then(
+      ([
+        { Route, Link },
+        { default: Breadcrumbs },
+        { default: TwentyFifteen },
+        { default: TwentySixteen },
+        { default: TwentySeventeen },
+        { default: TwentyEighteen },
+      ]) => {
+        this.setState({
+          Route,
+          Link,
+          Breadcrumbs,
+          TwentyFifteen,
+          TwentySixteen,
+          TwentySeventeen,
+          TwentyEighteen,
+        })
+      },
+    )
+  }
+  render() {
+    const {
+      Route,
+      Link,
+      Breadcrumbs,
+      TwentyFifteen,
+      TwentySixteen,
+      TwentySeventeen,
+      TwentyEighteen,
+    } = this.state
+    return (
+      <Container>
+        <Breadcrumbs />
+        <Route path="/blog/2015" component={TwentyFifteen} />
+        <Route path="/blog/2016" component={TwentySixteen} />
+        <Route path="/blog/2017" component={TwentySeventeen} />
+        <Route path="/blog/2018" component={TwentyEighteen} />
+        <Route
+          path="/blog"
+          exact
+          render={r =>
+            r.match && (
+              <Fragment>
+                <h3>Years:</h3>
+                <ul>
+                  <li>
+                    <Link to="/blog/2015">2015</Link>
+                  </li>
+                  <li>
+                    <Link to="/blog/2016">2016</Link>
+                  </li>
+                  <li>
+                    <Link to="/blog/2017">2017</Link>
+                  </li>
+                  <li>
+                    <Link to="/blog/2018">2018</Link>
+                  </li>
+                </ul>
+              </Fragment>
+            )
+          }
+        />
+      </Container>
+    )
+  }
+}
 export default Blog
